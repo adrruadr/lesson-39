bc19869ae3fade8a4d9f430e4682908bc67461b8
shyssirixius <ivmikvik@gmail.com>
2016-08-15 17:45:25 +0300

    implement article#show

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resource :contacts, only: [:new,:create]
 
 
-  get 'articles' => 'articles#new'
-  resources :articles, only: [:new,:create]
+
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
